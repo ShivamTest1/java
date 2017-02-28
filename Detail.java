5834253968735a18a320be08f524646ae3109c13
ShivamTest1 <shivam.ec082@gmail.com>
2017-02-28 22:22:45 +0530

    Update Detail.java

--- Detail.java
+++ Detail.java
@@ -1,4 +1,4 @@
-Class Detail
+class Detail
 {
 int salary,id;
 String name,company;
@@ -7,31 +7,21 @@ public void details(String location)
 salary=40000;
 id=75557;
 name="shivam";
-company="capgemini"
+company="capgemini";
 System.out.println("Salary is " +salary);
 System.out.println("id is " +id);
 System.out.println("name is " +name);
-System.out.println("Salary is " +company);
+System.out.println("company is " +company);
+System.out.println("location is " +location);
 }
-}
-Class D extends Detail
+
+public static void main(String[] args)
 {
-salary=60000;
-id=75558;
-name="rajan";
-company="ibm";
-public void details(String location)
-{
-System.out.println("Salary is " +salary);
-System.out.println("id is " +id);
-System.out.println("name is " +name);
-System.out.println("Salary is " +company);
-}
-public static void main(String args[])
-{
-D d = new D()
-d.details(bangalore);
-d.details(chennai);
+Detail d = new Detail();
+d.details("bangalore");
+
 }
 }
+
+
 
